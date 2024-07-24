fb492ac34181670b7ec32817700ae27e1a4d57a8
RECH_dev <llp835194@gmail.com>
2024-07-24 23:18:02 +0300

    я
    я

---
+++ Assets/ConnectManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class ConnectManager : MonoBehaviour
+{
+    public GameObject NoConnectionWarningWindows;
+
+    void Start()
+    {
+      if (Application.internetReachability == NetworkReachability.NotReachable)
+      {
+            NoConnectionWarningWindows.SetActive(true);
+            Debug.Log("Offline");
+        }
+      else
+      {
+            NoConnectionWarningWindows.SetActive(false);
+            Debug.Log("Online");
+        }
+    }
+}
