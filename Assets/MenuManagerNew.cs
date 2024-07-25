b4a47322378c2cc4a06c1a6318b6448b43af8ffc
RECH_dev <llp835194@gmail.com>
2024-07-26 00:33:52 +0300

    dfzdfhdf

--- Assets/MenuManagerNew.cs
+++ Assets/MenuManagerNew.cs
@@ -1,30 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MenuManagerNew : MonoBehaviour
 {
     public Animator animator;
+    public Animator animatorSettings;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MenuButton();
+        }
+        
+    }
 
     public void MenuButton()
     {
         animator.SetTrigger("DefaultMenu");
+        animatorSettings.SetTrigger("Close");
     }
-
     public void PlayButton()
     {
         animator.SetTrigger("PlayMenu");
+        animatorSettings.SetTrigger("Close");
     }
     public void QuitButton()
     {
         animator.SetTrigger("QuitMenu");
+        animatorSettings.SetTrigger("Close");
     }
     public void SettingsButton()
     {
         animator.SetTrigger("SettingsMenu");
-    }
-    void Update()
-    {
-
+        animatorSettings.SetTrigger("Open");
     }
 }
