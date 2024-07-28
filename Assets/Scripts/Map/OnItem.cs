a3c857e779452d343fe556d9f2c74ad513fdda3e
Karton <131799749+Karteam@users.noreply.github.com>
2024-07-28 13:09:48 +0300

    dw
    dw

--- Assets/Scripts/Map/OnItem.cs
+++ Assets/Scripts/Map/OnItem.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TVItem : MonoBehaviour
+public class OnItem : MonoBehaviour
 {
     public GameObject video;
 
     [Range(0, 1)]
     public float ChanceOfgo = 1f;
 
+    public bool IsOn;
     
     private void Start()
     {
-
+        IsOn = false;
         video.SetActive(false);
         if (Random.value > ChanceOfgo)
         {
-            StartCoroutine(Go());
+            IsOn = true;
+            
         }
       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ((other.CompareTag("Player")) && IsOn)
+        {
+            StartCoroutine(Go());
+        }
     }
 
     public IEnumerator Go()
     {
       
-        yield return new WaitForSeconds(Random.Range(20,60));
+        yield return new WaitForSeconds(Random.Range(0.1f,0.6f));
         video.SetActive(true);
 
 
