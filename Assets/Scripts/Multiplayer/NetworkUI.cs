989d97965bfd11814ca272460c0b4ab50e28a3b7
RECH_dev <llp835194@gmail.com>
2024-08-24 21:57:01 +0300

    ghjhgj
    hgjhjg

--- Assets/Scripts/Multiplayer/NetworkUI.cs
+++ Assets/Scripts/Multiplayer/NetworkUI.cs
@@ -2,57 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class NetworkUI : MonoBehaviour
 {
     [SerializeField] private Button startHostButton;
     [SerializeField] private Button startServerButton;
-
     [SerializeField] private Button startClientButton;
 
     [SerializeField] private GameObject buttonsPanel;
-
     [SerializeField] private GameObject menuCamera;
+
+    [SerializeField] private TMP_InputField ipAddressInputField; // Поле для ввода IP-адреса
+
     // Start is called before the first frame update
     void Start()
     {
-        startClientButton.onClick.AddListener(() =>
+        startClientButton.onClick.AddListener(StartClient);
+        startServerButton.onClick.AddListener(StartServer);
+        startHostButton.onClick.AddListener(StartHost);
+    }
+
+    private void StartClient()
+    {
+        string ipAddress = ipAddressInputField.text;
+        if (NetworkManager.Singleton.StartClient())
         {
-            if (NetworkManager.Singleton.StartClient())
-            {
-                OnSuccessConnection();
-                print("Client started!");
-            }
-            else
-            {
-                print("Client not started");
-            }
-        });
-        startServerButton.onClick.AddListener(() =>
+            OnSuccessConnection();
+            print("Client started!");
+        }
+        else
         {
-            if (NetworkManager.Singleton.StartServer())
-            {
-                OnSuccessConnection();
-                print("Server started!");
-            }
-            else
-            {
-                print("Server not started");
-            }
-        });
-        startHostButton.onClick.AddListener(() =>
+            print("Client not started");
+        }
+    }
+
+    private void StartServer()
+    {
+        if (NetworkManager.Singleton.StartServer())
         {
-            if (NetworkManager.Singleton.StartHost())
-            {
-                OnSuccessConnection();
-                print("Host started!");
-            }
-            else
-            {
-                print("Host not started");
-            }
-        });
+            OnSuccessConnection();
+            print("Server started!");
+        }
+        else
+        {
+            print("Server not started");
+        }
+    }
+
+    private void StartHost()
+    {
+        if (NetworkManager.Singleton.StartHost())
+        {
+            OnSuccessConnection();
+            print("Host started!");
+        }
+        else
+        {
+            print("Host not started");
+        }
     }
 
     private void OnSuccessConnection()
