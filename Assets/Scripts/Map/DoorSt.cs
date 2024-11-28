a98f8204da7f77c16398e8b2e6cf99e823a93c27
ic3nt <info.relkvin@gmail.com>
2024-11-29 00:28:41 +0300

    ef
    ef

--- Assets/Scripts/Map/DoorSt.cs
+++ Assets/Scripts/Map/DoorSt.cs
@@ -6,19 +6,24 @@ using Unity.Netcode;
 public class DoorSt : NetworkBehaviour
 {
     public AudioSource audioSource;
+    public Material doorMaterial;
 
-    public bool IsLocked; // Добавлена точка с запятой здесь
+    public Color lockedColor = Color.red;
+    public Color unlockedColor = Color.green;
+
+    public bool IsLocked;
 
     [Range(0, 1)]
     public float ChanceOfLock = 0.5f;
 
     public override void OnNetworkSpawn()
     {
-        // Выполняется только для сервера
         if (IsOwner)
         {
             IsLocked = Random.value > ChanceOfLock;
         }
+
+        UpdateControllerDoorColor();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,14 +32,9 @@ public class DoorSt : NetworkBehaviour
         {
             if (other.TryGetComponent<Key>(out var key))
             {
-                // Проверяем, является ли объект сервером
-                
-
-                // Уничтожаем объект ключа
                 Destroy(key.KeyObject);
                 Debug.Log("Ключ использован для разблокировки двери");
 
-                // Вызываем RPC для разблокировки двери
                 UnlockDoorServerRpc();
             }
         }
@@ -46,9 +46,9 @@ public class DoorSt : NetworkBehaviour
         if (IsLocked)
         {
             Debug.Log("UnlockDoorServerRpc вызван");
-            IsLocked = false; // Разблокируем дверь на сервере
-            audioSource?.Play(); // Проигрываем звук разблокировки
-            UpdateClientsClientRpc(); // Уведомляем всех клиентов о разблокировке
+            IsLocked = false;
+            audioSource?.Play();
+            UpdateClientsClientRpc();
             Debug.Log("Дверь разблокирована");
         }
         else
@@ -60,7 +60,22 @@ public class DoorSt : NetworkBehaviour
     [ClientRpc]
     private void UpdateClientsClientRpc()
     {
-        IsLocked = false; // Разблокируем дверь на клиентах
-        audioSource?.Play(); // Проигрываем звук для всех клиентов
+        IsLocked = false;
+        audioSource?.Play();
+        UpdateControllerDoorColor();
+
+    }
+
+    private void UpdateControllerDoorColor()
+    {
+        if (doorMaterial != null)
+        {
+            Color targetColor = IsLocked ? lockedColor : unlockedColor;
+
+            doorMaterial.SetColor("_Color", targetColor);
+
+            doorMaterial.EnableKeyword("_EMISSION");
+            doorMaterial.SetColor("_EmissionColor", targetColor);
+        }
     }
 }
