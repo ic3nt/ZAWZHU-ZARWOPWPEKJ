8e3f42ff7d678c9c3c0bd798d152898652bdfea6
Karton <131799749+Karteam@users.noreply.github.com>
2025-08-18 17:00:23 +0300

    Light fonarikk

--- Assets/!Deadly Devastation/Scripts/Player/PlayerLight.cs
+++ Assets/!Deadly Devastation/Scripts/Player/PlayerLight.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class PlayerLight : MonoBehaviour
 {
-  
+    public GameObject Light;
+
+
+
+    private void Start()
+    {
+        Light.SetActive(false);
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //an.SetBool("IsLighterOn", !an.GetBool("IsLighterOn"));
+
+            Light.SetActive(!Light.active);
+            AudioManager.Instance.Play("--");
+        }
+
+    }
+
 }
